c0062e7dcd3975395db9a87aa170c2fcd7c9df52
Joao Carlos <joaoslz@gmail.com>
2018-03-14 16:21:28 -0300

    Primeiro commit

---
+++ src/test/java/aula/topicos2/p01/P01ApplicationTests.java
@@ -0,0 +1,16 @@
+package aula.topicos2.p01;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class P01ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
