39a84e0c1e39dae240ef34f9d066f41022c6a0dd
Joao Carlos <joaoslz@gmail.com>
2018-03-15 18:24:06 -0300

    Adicionada as dependencias do JPA e MySQL

--- src/main/java/aula/topicos2/p01/P01Application.java
+++ src/main/java/aula/topicos2/p01/P01Application.java
@@ -1,13 +1,30 @@
 package aula.topicos2.p01;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class P01Application {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(P01Application.class, args);
-		System.out.println("Meu primeiro Projeto com Spring Boot ...");
+		System.out.println("Iniciando a aplicação ...");
 	}
+	
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+	
+		return (args) -> { 
+			System.out.println("Testando 1 2 3 ..."); 
+		};
+		
+	}
+	
+	
+	
+	
 }
